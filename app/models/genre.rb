6f28a83f8ef625efac99eb26bd8afda753efd84f
Aida Abdikulova <abdikulova@gmail.com>
2020-02-10 05:51:41 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -16,6 +16,6 @@ class Genre < ActiveRecord::Base
     # return an array of strings containing every musician's name
     array = []
     array << self.artists
-    array.name
+    array
   end
 end
